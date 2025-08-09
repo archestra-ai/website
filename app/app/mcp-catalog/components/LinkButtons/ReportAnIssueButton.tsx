c50f43bfdd05806261736f35c89d0be815423424
Joey Orlando <joseph.t.orlando@gmail.com>
2025-08-09 16:30:12 -0400

    wip

--- app/app/mcp-catalog/components/LinkButtons/ReportAnIssueButton.tsx
+++ app/app/mcp-catalog/components/LinkButtons/ReportAnIssueButton.tsx
@@ -38,9 +38,9 @@ const ReportAnIssueButton = ({
     <LinkButton link={`${newIssueUrl}?${issueUrlParams}`} additionalClassNames={colorClassNames} {...linkButtonProps}>
       <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
           d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
