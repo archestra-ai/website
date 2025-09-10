8cf44d25ec514759c70ff7c6828a6dfd02f8fd0c
Matvey-Kuk <motakuk@gmail.com>
2025-09-10 19:39:57 +0100

    TOS and PP

---
+++ app/app/privacy/privacy-content.tsx
@@ -0,0 +1,144 @@
+'use client';
+
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+const PRIVACY_POLICY_CONTENT = `
+# Privacy Policy
+
+Last updated: 10 September 2025
+
+# What personal information we collect, why we collect it, and how it is used
+
+In order to ensure transparency and give you more control over your personal information, this privacy policy (“Privacy Policy”) governs how we, Archestra Inc. (“Archestra”, “we”, “our” or “us”) use, collect and store personal information that we collect or receive from or about you (“you”) in connection with https://www.archestra.ai/ (“Website”), Archestra platform (“Platform”), Desktop Application, and the services provided therein (the Website, Platform and provided services, collectively, the “Services”).
+
+We greatly respect your privacy, which is why we make every effort to provide a platform that would live up to the highest user privacy standards. Please read this Privacy Policy carefully, so you can fully understand our practices in relation to personal information.
+
+Important note: Nothing in this Privacy Policy is intended to limit in any way your statutory rights, including your rights to a remedy or other means of enforcement.
+
+Archestra’s mission is to provide you with an open-source MCP AI orchestration platform.
+
+This Privacy Policy can be updated from time to time and, therefore, we ask you to check back periodically for the latest version of this Privacy Policy. If we implement material changes in the way we use your information, in a manner that is different from that stated at the time of collection, we will notify you by posting a notice on our Website or by other means and take any additional steps as required by applicable law.
+
+# What personal information we collect, why we collect it, and how it is used
+
+We process the following personal information:
+
+1. Information provided through the Services. We collect personal information that you voluntarily provide, such as your full name, email address, Gmail or GitHub account details as well as any other information that you decide to provide us. We also collect the contact and billing information of our customers.
+2. Information provided through the Website. When you use the Website, we collect and process your full name and email address when you interact with the Website, for example, when you register for our newsletter and updates.
+3. Information automatically collected. We automatically collect certain information through your use of Archestra's Services and Website, such as cookie, pixels, tracking technologies and similar identifiers (“Technologies”), your Internet protocol (IP) address, and other device identifiers that are automatically assigned to your device, browser type and language, geo-location information, hardware type, operating system, internet service provider and other information about actions taken through the use of the Services.  
+4. Information provided through the Platform. When you use the Platform as an authorized user, we collect and process your Gmail and/or GitHub credentials to allow you to sign-up/login and access the Platform. When you choose to connect our Platform to your other providers, depending on the configuration of those systems, the Platform will receive user login credentials or an anonymous identifier or token. When you interact with the Platform, the system collects the traffic information needed to deliver your actions and instructions over the Internet. Personal information about you includes IP addresses and URLs used to deliver content to you, and the Platform collects other system information to allow the system to recognize your devices. The Platform uses cookies to remember your preferences and/or support the use of digital rights management systems.
+5. Information you provide to us in person. For example, when you visit one of our exhibition booths or attend one of our events, provide us with your contact details. We will use this information to answer your enquiries or provide additional information to you.
+6. Information we collect from online interactions. For example, if you attend a webinar, contact us via social media or otherwise interact with our business, including as a representative of a current / prospective customer, supplier or partner, we track and make a record of those interactions, which contain your contact details, such as full name, email address, messages and any other information that you decide to provide us with.
+
+# We process information for the following purposes
+
+1. To provide you with the Services. We will use the information, including, without limitation, for the following purposes: (i) allow you to create an account and sign in to our platform; (ii) to provide you the Services and to process your requests; (iii) communicate with you about your use of the Services and for support purposes; (iv) fulfill any instruction and/or request made by you in the context of the Services; (vii) to allow you to create more users and administrate your users; and (viii) to generally administer and improve the Services;
+2. For Administrative Purposes. Archestra will use your information (i) to respond to your questions, comments, and other requests for customer support, or information, including information about potential or future services; (ii) to provide you with the Archestra Services; (iii) for internal quality control purposes; (iv) to establish a business relationship; and (v) to generally administer Archestra Services;
+3. To Market our Website and Services. Archestra will use information to market our Services. Such use includes (i) notifying you about offers and services that may be of interest to you; (ii) developing and marketing new products and services, and to measure interest in Archestra's Services; (iii) other purposes disclosed at the time you provide information; and (iv) as you otherwise consent;
+4. Security purposes. Some of the abovementioned information will be used for detecting, taking steps to prevent and prosecuting fraud or other illegal activity; to identify and repair errors; to conduct audits; and for security purposes. Information may also be used to comply with applicable laws, with investigations performed by the relevant authorities, law enforcement purposes, and/or to exercise or defend legal claims.
+5. De-identified and Aggregated Information Use. In certain cases, we will anonymize or de-identify your Information and further use it for internal and external purposes, including, without limitation, to analyze and improve Archestra services (including through the use of artificial intelligence) and for research purposes. We may use this anonymous or de-identified information and/or disclose it to third parties without restrictions (for example, in order to improve our services and enhance your experience with them and/or to develop new product features and improve existing offerings).
+6. Cookies and Similar Technologies. We, as well as third parties that provide content, advertising, or other functionality on the Archestra Website and Services, use Technologies to automatically collect information through the Website and Services. We use Technologies that are essentially small data files placed on your device that allow us to record certain pieces of information whenever you visit or interact with the Website and Services. If you would like to opt out of the cookies and similar technologies we employ on the Website and Services, you may do so by blocking, deleting, or disabling them as your browser or device permits.
+
+# How we protect and retain your personal information
+
+1. Security. We have implemented and maintain reasonable technical, organizational, and security measures designed to protect your information. However, please note that we cannot guarantee that the information will not be compromised as a result of unauthorized penetration to our servers. As the security of information depends in part on the security of the computer, device, or network you use to communicate with us and the security you use to protect your user IDs and passwords, please make sure to take appropriate measures to protect this information.
+2. Retention of your information. Your information will be stored until we delete our records, and we proactively delete it, or if you send a valid deletion request. Please note that in some circumstances we may store your information for longer periods of time, for example (i) where we are required to do so in accordance with legal, regulatory, tax or accounting requirements, or (ii) for us to have an accurate record of your dealings with us in the event of any complaints or challenges, and/or (iii) if we reasonably believe there is a prospect of litigation relating to your information or dealings.
+
+# How we share your personal information
+
+We may share your information as follows:
+
+The information Archestra gathers is shared with our partners and other third parties.
+We also share information with our affiliated companies about you.
+We use third-party service providers to process your information for the purposes outlined above, including, without limitation:
+With cloud service providers  for hosting purposes;
+With websites and web content creation platforms in order to help us manage our Website;
+With email providers, marketing, CRM, other similar tool providers;
+With analytic companies, in order to help us understand and analyze information we collect in accordance with this policy; and
+To the extent necessary, with regulators, courts, banks or competent authorities, to comply with applicable laws, regulations and rules (including, without limitation, federal, state or local laws), and requests of law enforcement, regulatory and other governmental agencies or if required to do so by court order, as well as for internal compliance procedures and to protect the safety, security, and integrity of Archestra, our Services, customers, employees, property, and the public.
+If, in the future, we sell or transfer, or we consider selling or transferring, some or all of our business, shares or assets to a third party, we will disclose your information to such third party (whether actual or potential) in connection with the foregoing events (including, without limitation, our current or potential investors). In the event that we are acquired by, or merged with, a third-party entity, or in the event of bankruptcy or a comparable event, we reserve the right to transfer, disclose or assign your information in connection with the foregoing events.
+Where you have otherwise provided your consent to us for sharing or transferring your information.
+
+# Your privacy right
+
+The following rights (which may be subject to certain exemptions or derogations) shall apply to certain individuals (some of which only apply to individuals protected by specific laws):
+You have the right to withdraw consent to the processing, where consent is the basis of processing.
+
+You have the right to access the personal information that we hold and request further details about how we process it, under certain conditions.
+
+- You have the right to demand rectification of inaccurate personal information about you. We will promptly correct any information found to be incorrect.
+- You have the right to object to unlawful data processing under certain conditions.
+- You have the right to the erasure of past data about you (your “right to be forgotten”) under certain conditions.
+- You have the right to demand that we restrict the processing of your personal information, under certain conditions, if you believe we have exceeded the legitimate basis for processing, the processing is no longer necessary, or if you believe your personal information is inaccurate.
+- You have the right to data portability of personal information concerning you that you provided us in a structured, commonly used, and machine-readable format, subject to certain conditions.
+- The personal information we collect is not used for automated decision-making and profiling, except for automated processes in the context of marketing. As stated above, you can opt out of direct marketing by Archestra by contacting Archestra directly or by following the instructions through the unsubscribe options in our email messages.
+- You can exercise your rights by contacting us at matvey@archestra.ai. You may use an authorized agent to submit a request on your behalf if you provide the authorized agent with written permission signed by you. To protect your privacy, we may take steps to verify your identity before fulfilling your request. Subject to legal and other permissible considerations, we will make every reasonable effort to honor your request promptly in accordance with applicable law or inform you if we require further information in order to fulfil your request. When processing your request, we may ask you for additional information to confirm or verify your identity and for security purposes before processing and/or honoring your request. We reserve the right to charge a fee permitted by law, for instance if your request is manifestly unfounded or excessive. In the event that your request would adversely affect the rights and freedoms of others (for example, would impact the duty of confidentiality we owe to others) or if we are legally entitled to deal with your request in a different way than initial requested, we will address your request to the maximum extent possible, all in accordance with applicable law.
+- Deleting your account: Should you ever decide to delete your account, you may do so by emailing matvey@archestra.ai. If you terminate your account, any association between your account and personal information we store will no longer be accessible through your account.
+- Marketing emails – opt-out: if you receive an unwanted email from us, you can use the unsubscribe link found at the bottom of the email to opt out of receiving future emails, and we will process your request within a reasonable time after receipt.
+
+# International transfers of personal information
+
+We store the personal information with the following storing company - [*]GCP – Iowa – United States.
+
+In order to run our business and provide our Website and Services to you, we transfer personal information to certain countries around the world, including to our affiliates and service providers, many of whom are located outside of your jurisdiction. Therefore, your personal information may be processed in countries with privacy laws that are different from privacy laws in your country. Whenever we make such transfers, we will use commercially reasonable efforts to implement an appropriate level of protection to your Personal Data by implementing at least one of the following safeguards:
+
+making sure the destination country has been deemed to provide an adequate level of protection for personal information; and/or
+
+by executing implement data onward transfer instruments such as data processing and protection agreements.
+
+1. USE BY CHILDREN. We do not offer our products or services for use by children and, therefore, we do not knowingly collect information from, and/or about children under the age of 18. If you are under the age of 18, do not provide any information to us without the involvement of a parent or a guardian. In the event that we become aware that you provide information in violation of applicable privacy laws, we reserve the right to delete it. If you believe that we might have any such information, please contact us at matvey@archestra.ai.
+2. INTERACTION WITH THIRD-PARTY PRODUCTS. We enable you to interact with third-party websites, software applications and products or services that are not owned, or controlled, by us (each, a “Third Party Service”). We are not responsible for the privacy practices or the content of such Third Party Services. Please be aware that Third Party Services can collect information from you. Accordingly, we encourage you to read the terms and conditions and privacy policies of each Third Party Service.
+
+# Analytic tools/cookies
+We reserve the right to access, read, preserve, and disclose any information that we obtain in connection with the Site, and your use thereof, as we reasonably believe is necessary to: (i) satisfy any applicable law, regulation, legal process, subpoena or governmental request; (ii) enforce these Terms, including to investigate potential violations of them; (iii) detect, prevent or otherwise address fraud, security or technical issues; (iv) respond to user support requests; or (v) protect the rights, property or safety of Archestra, its users or the public.
+
+- Google Analytics. The Website uses “Google Analytics“ and “PostHog” to collect information about use of the Website and the Platform. PostHog collects information such as how often users visit this Website, what pages they visit when they do so, and what other websites they used prior to coming to this Website. We use the information we get from PostHog to maintain and improve our Services. You can read more about PostHog and the way they treat your personal information here: https://posthog.com/docs/privacy and here: https://posthog.com/docs/privacy/gdpr-compliance.
+
+We reserve the right to remove or add new analytic tools, cookies, pixels, and other tracking technologies.
+
+# Our California Do Not Track Notice
+Do Not Track (“DNT”) is a privacy preference that users can set in certain web browsers. Please note that we do not respond to or honor DNT signals or similar mechanisms transmitted by web browsers
+
+# Contact us
+If you have any questions, concerns, or complaints regarding our compliance with this notice and the data protection laws, or if you wish to exercise your rights, we encourage you to first contact us at matvey@archestra.ai.
+
+**Archestra Inc.**  
+Email: matvey@archestra.ai  
+Website: https://archestra.ai
+`;
+
+export default function PrivacyContent() {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        p: ({ node, ...props }) => <p {...props} className="text-base text-gray-700 leading-relaxed mb-4" />,
+        h1: ({ node, ...props }) => (
+          <h1 {...props} className="text-4xl font-bold text-gray-900 mb-8" />
+        ),
+        h2: ({ node, ...props }) => (
+          <h2 {...props} className="text-2xl font-semibold text-gray-900 mb-4 mt-8" />
+        ),
+        h3: ({ node, ...props }) => <h3 {...props} className="text-xl font-semibold text-gray-900 mb-3 mt-6" />,
+        ul: ({ node, ...props }) => <ul {...props} className="list-disc list-inside mb-4 space-y-2 pl-4" />,
+        ol: ({ node, ...props }) => (
+          <ol {...props} className="list-decimal list-inside mb-4 space-y-2 pl-4" />
+        ),
+        li: ({ node, ...props }) => <li {...props} className="text-base text-gray-700 leading-relaxed" />,
+        a: ({ node, ...props }) => (
+          <a {...props} className="text-blue-600 hover:underline" />
+        ),
+        strong: ({ node, ...props }) => <strong {...props} className="font-semibold text-gray-900" />,
+        blockquote: ({ node, ...props }) => (
+          <blockquote
+            {...props}
+            className="border-l-4 border-gray-300 pl-6 my-6 text-base text-gray-600 italic"
+          />
+        ),
+      }}
+    >
+      {PRIVACY_POLICY_CONTENT}
+    </ReactMarkdown>
+  );
+}
