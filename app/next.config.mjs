619de3eb7d98b9e66c8b97fc55d54cbdf9405af3
Joey Orlando <joey@archestra.ai>
2025-09-11 17:51:09 -0400

    chore: remove posthog next.js "rewrites" (#113)

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -33,22 +33,6 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://eu-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://eu.i.posthog.com/:path*',
-      },
-      {
-        source: '/ingest/flags',
-        destination: 'https://eu.i.posthog.com/flags',
-      },
-    ];
-  },
   skipTrailingSlashRedirect: true,
 };
 
