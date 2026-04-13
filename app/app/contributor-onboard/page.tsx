2233956d43ec5099478698bb81bc9bca1b5380a5
Ildar Iskhakov <ildar6v@gmail.com>
2026-04-13 12:43:19 +0100

    lint

--- app/app/contributor-onboard/page.tsx
+++ app/app/contributor-onboard/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Script from 'next/script';
 import { useSearchParams } from 'next/navigation';
+import Script from 'next/script';
 import { Suspense } from 'react';
 
 import Footer from '@components/Footer';
